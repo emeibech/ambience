083f6b26ff93fcd704e9c54e9a4bd966cae4a010
emeibech <98073805+emeibech@users.noreply.github.com>
2024-01-26 01:05:06 +0800

    Merge props and add className to button props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,13 @@
 import { type AriaButtonProps, useButton } from '@react-aria/button';
-import { useHover, useFocusVisible } from '@react-aria/interactions';
+import { useHover } from '@react-aria/interactions';
 import { useRef } from 'react';
+import { useFocusRing } from '@react-aria/focus';
+import { mergeProps } from '@react-aria/utils';
 
 /* why this? because css pseudoclasses doesn't have consistent behavior
 across different browsers. react-aria interaction hooks solves it. */
 interface ButtonProps extends AriaButtonProps {
+  className?: string | undefined;
   hoverStyle?: string | undefined;
   pressedStyle?: string | undefined;
 }
@@ -13,19 +16,19 @@ export function Button(props: ButtonProps) {
   const ref = useRef(null);
   const { buttonProps, isPressed } = useButton(props, ref);
   const { hoverProps, isHovered } = useHover({});
-  const { isFocusVisible } = useFocusVisible();
-  const { children, hoverStyle, pressedStyle } = props;
+  const { isFocusVisible, focusProps } = useFocusRing();
+  const { children, hoverStyle, pressedStyle, className } = props;
+  const mergedProps = mergeProps(buttonProps, hoverProps, focusProps);
 
   return (
     <button
-      {...buttonProps}
-      {...hoverProps}
+      {...mergedProps}
       ref={ref}
       className={`
       ${!isFocusVisible ? 'outline-none' : ''}
       ${isHovered ? hoverStyle : ''}
       ${isPressed ? pressedStyle : ''}
-      px-4 py-2
+      ${className ? className : 'px-4 py-2'}
     `}
     >
       {children}
