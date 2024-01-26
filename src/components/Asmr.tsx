033a7f78cf6c9f39f5d984cf34869894cdde3e37
emeibech <98073805+emeibech@users.noreply.github.com>
2024-01-26 08:21:37 +0800

    Change padding break point

--- src/components/Asmr.tsx
+++ src/components/Asmr.tsx
@@ -14,7 +14,7 @@ export default function Asmr({ name }: AsmrProps) {
   return (
     <div
       className={`
-        max-w-[960px] min-w-[min(90vw,_640px)] p-4 xs:p-8 rounded-2xl
+        max-w-[960px] min-w-[min(90vw,_640px)] p-4 sm:p-8 rounded-2xl
         backdrop-blur backdrop-brightness-[0.33]
       `}
       data-name={name}
