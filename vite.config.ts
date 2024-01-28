55a57b1c943328b8e6642383308907316b427d7a
emeibech <98073805+emeibech@users.noreply.github.com>
2024-01-28 14:32:36 +0800

    Complete pwa requirements

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,43 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        theme_color: '#2A0134',
+        background_color: '#2A0134',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        short_name: 'Emeibech Ambience',
+        description: 'Emeibech Ambience',
+        name: 'Emeibech Ambience',
+        icons: [
+          {
+            src: '/192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: '/512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
+  ],
 });
