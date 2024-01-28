0b3466c3f69d5f629e5d530eb4f3f0dcc7ffdfaa
emeibech <98073805+emeibech@users.noreply.github.com>
2024-01-28 13:35:59 +0800

    Provision PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    manifest: true,
-  },
+  plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
 });
